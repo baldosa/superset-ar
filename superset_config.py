1b4c266be0db80cbd6084a36daae739e5f5a05d6
baldosa <ema@guillen.com.ar>
2022-11-25 16:55:03 -0300

    config con envs

--- superset_config.py
+++ superset_config.py
@@ -1,33 +1,18 @@
-from flask import Flask, redirect
+import os
+from flask import Flask, redirect, session
+
 
 from flask_appbuilder import expose, IndexView
 
 from superset.superset_typing import FlaskResponse
 
-# source: https://github.com/apache/incubator-superset/pull/1866#issuecomment-347310860
-class ReverseProxied(object):
+from datetime import timedelta
+from mapa_view.views import mapa_bp
 
-    def __init__(self, app):
-        self.app = app
+# mapbox api
+MAPBOX_API_KEY = os.getenv('MAPBOX_API_KEY')
 
-    def __call__(self, environ, start_response):
-        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
-        if script_name:
-            environ['SCRIPT_NAME'] = script_name
-            path_info = environ['PATH_INFO']
-            if path_info.startswith(script_name):
-                environ['PATH_INFO'] = path_info[len(script_name):]
-
-        scheme = environ.get('HTTP_X_SCHEME', '')
-        if scheme:
-            environ['wsgi.url_scheme'] = scheme
-        return self.app(environ, start_response)
-
-
-#ADDITIONAL_MIDDLEWARE = [ReverseProxied, ]
-
-
-MAPBOX_API_KEY = ''
+# cache config
 CACHE_CONFIG = {
     'CACHE_TYPE': 'RedisCache',
     'CACHE_DEFAULT_TIMEOUT': 300,
@@ -35,7 +20,8 @@ CACHE_CONFIG = {
     'CACHE_REDIS_HOST': 'redis',
     'CACHE_REDIS_PORT': 6379,
     'CACHE_REDIS_DB': 1,
-    'CACHE_REDIS_URL': 'redis://redis:6379/1'}
+    'CACHE_REDIS_URL': 'redis://redis:6379/1'
+}
 
 FILTER_STATE_CACHE_CONFIG = {
     'CACHE_TYPE': 'RedisCache',
@@ -47,45 +33,101 @@ FILTER_STATE_CACHE_CONFIG = {
 EXPLORE_FORM_DATA_CACHE_CONFIG = {
     'CACHE_TYPE': 'RedisCache',
     'CACHE_DEFAULT_TIMEOUT': 86400,
-    'CACHE_KEY_PREFIX': 'superset_filter_',
+    'CACHE_KEY_PREFIX': 'superset_form_',
     'CACHE_REDIS_URL': 'redis://redis:6379/3'
 }
+
+# database
 SQLALCHEMY_DATABASE_URI = \
-    'postgresql+psycopg2://superset:superset@postgres:5432/superset'
+    f'postgresql+psycopg2://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@postgres:5432/superset'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+# language options
 BABEL_DEFAULT_LOCALE = "es"
 BABEL_DEFAULT_FOLDER = "superset/translations"
 LANGUAGES = {
     "es": {"flag": "ar", "name": "Spanish"},
 }
-SECRET_KEY = 'changmemememe'
 
-# Config custom del coso
+# Config custom para nginx
 ENABLE_PROXY_FIX=True
-#SUPERSET_WEBSERVER_PROTOCOL = "https"
-ENABLE_JAVASCRIPT_CONTROLS = True
-PROXY_FIX_CONFIG = {"x_for": 1, "x_proto": 1, "x_host": 0, "x_port": 0, "x_prefix": 1}
+PROXY_FIX_CONFIG = {"x_for": 1, "x_proto": 1, "x_host": 1, "x_port": 1, "x_prefix": 0}
+HTTP_HEADERS = {'X-Frame-Options': 'ALLOWALL'}
 
-# no registration
-AUTH_USER_REGISTRATION = False
-# redirect
+# custom path público
+BLUEPRINTS = [mapa_bp]
+
+# redirect al dashboard base
+WELCOME_PAGE_REDIRECT_DEFAULT="/mapa"
+WELCOME_PAGE_REDIRECT_ADMIN="/dashboard/list/"
+
+WELCOME_PAGE_REDIRECT_BY_ROLE={
+  'Gamma': '/dashboard/list/',
+  'MDS': '/superset/dashboard/84/',
+}
+
+
+# Change welcome page
+# https://stackoverflow.com/a/69930056/1760643
 class SupersetDashboardIndexView(IndexView):
     @expose("/")
     def index(self) -> FlaskResponse:
-        return redirect("/superset/dashboard/2/")
+        from superset.views.base import is_user_admin
+        from superset import security_manager
+        user_roles = security_manager.get_user_roles()
+
+        if is_user_admin():
+            return redirect(WELCOME_PAGE_REDIRECT_ADMIN)
+        else:
+            for role in user_roles:
+                role_name = role.name
+
+                if role_name in WELCOME_PAGE_REDIRECT_BY_ROLE:
+                    return redirect(WELCOME_PAGE_REDIRECT_BY_ROLE[role_name])
+
+            return redirect(WELCOME_PAGE_REDIRECT_DEFAULT)
 
 FAB_INDEX_VIEW = f"{SupersetDashboardIndexView.__module__}.{SupersetDashboardIndexView.__name__}"
 
+
+# set up max age of session to 24 hours
+def make_session_permanent():
+    '''
+    Enable maxAge for the cookie 'session'
+    '''
+    session.permanent = True
+
+# set up max age of session to 365 days 
+PERMANENT_SESSION_LIFETIME = timedelta(days=365)
+def FLASK_APP_MUTATOR(app: Flask) -> None:
+    app.before_request_funcs.setdefault(None, []).append(make_session_permanent)
+
 # base url
-#WEBDRIVER_BASEURL = "https://reportes.mds.gob.ar/"
 # This is for internal use, you can keep http
 WEBDRIVER_BASEURL="http://superset:8088"
 # This is the link sent to the recipient, change to your domain eg. https://superset.mydomain.com
-WEBDRIVER_BASEURL_USER_FRIENDLY="https://reportes.mds.gob.ar"
+WEBDRIVER_BASEURL_USER_FRIENDLY=os.getenv('SUPERSET_URL')
 
 # app title
 APP_NAME = 'Mapa de Inversión Social - Ministerio de Desarrollo Social'
+
+
+# no registration
+AUTH_USER_REGISTRATION = False
+
+# map controls
+ENABLE_JAVASCRIPT_CONTROLS = True
+
+# Feature flags
 FEATURE_FLAGS = {
     "VERSIONED_EXPORT": True,
     "DASHBOARD_RBAC": True,
+    "ENABLE_JAVASCRIPT_CONTROLS": True,
+    "EMBEDDED_SUPERSET": True,
+    "GENERIC_CHART_AXES": True,
+    "DISABLE_LEGACY_DATASOURCE_EDITOR": False,
 }
+
+PUBLIC_ROLE_LIKE = "Gamma"
